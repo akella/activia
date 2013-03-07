06c58ea482cec94e4f35d466a043aff9c229ec1f
Yevgen <godun.yevgen@gmail.com>
2013-03-07 16:32:31 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -9,4 +9,21 @@ $(document).ready(function() {
         }
 	});
 
+    $(".scroll__main").scrollable().navigator({
+        navi: ".scroll__nav",
+        naviItem: 'button',
+        activeClass: 'is-active',
+        indexed: true
+    });
+
+    $(".scroll__slide").click(function(){
+        if($(this).hasClass("is-active")) {
+             $(this).removeClass("is-active");
+        }
+        else {
+            $(this).addClass("is-active");
+        }
+       
+    });
+
 });
